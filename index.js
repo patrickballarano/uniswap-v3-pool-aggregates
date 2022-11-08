934671f335e84b4481b5fccde15388350155c399
Patrick Ballarano <pballarano@investvoyager.com>
2022-11-08 16:49:56 -0500

    Making progress on observing uniswap contract

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const axios = require('axios')
 const ethers = require('ethers')
 const BigNumber = require('bignumber')
 const abi = require('@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json')
+const erc20abi = require('./erc20abi.json')
 const INFURA_URL = process.env.INFURA_URL
 const UNISWAP_SUBGRAPH_URL = 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3'
 const provider = new ethers.providers.JsonRpcProvider(INFURA_URL)
 const poolAddress = '0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8'
 const poolContract = new ethers.Contract(poolAddress, abi.abi, provider)
-
 
 async function main() {
   const res = await axios.post(UNISWAP_SUBGRAPH_URL, { query: `
@@ -25,11 +25,13 @@ async function main() {
     }
   }
   ` })
-  const pools = res.data.data.pools
+  const pools = res.data.data.pools.map(pool => pool.id)
 
   console.log(pools)
-
-  const volume = await provider.getBalance("0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8")
+  const volume = await provider.getBalance(pools[0])
+  const pool = new ethers.Contract(pools[0], abi.abi, provider)
+  console.log(await pool.token0())
+  console.log(await pool.token1())
   console.log(ethers.utils.formatEther(volume))
 
 }
